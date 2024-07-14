8cd8fc35be4be07d045a1fa498df4c261cf87868
jazal ahamed <jazalahamed19@gmail.com>
2024-07-14 16:34:45 +0530

    Path segments for return

--- DijkstraRouteFinder.Models/ShortestPathResponseDto.cs
+++ DijkstraRouteFinder.Models/ShortestPathResponseDto.cs
@@ -4,4 +4,11 @@ public class ShortestPathResponseDto
 {
     public List<string> NodeNames { get; set; }
     public int Distance { get; set; }
+    public List<PathSegmentDto> PathSegments { get; set; }
+
+    public ShortestPathResponseDto()
+    {
+        NodeNames = new List<string>();
+        PathSegments = new List<PathSegmentDto>();
+    }
 }
--- DijkstraRouteFinder.Services/ShortestPathService.cs
+++ DijkstraRouteFinder.Services/ShortestPathService.cs
@@ -4,7 +4,7 @@ namespace DijkstraRouteFinder.Services;
 
 public class ShortestPathService : IShortestPathService
 {
-    public ShortestPathResponseDto ShortestPath(string fromNodeName, string toNodeName, List<Node> graphNodes)
+    public ShortestPathResponseDto ShortestPathV1(string fromNodeName, string toNodeName, List<Node> graphNodes)
     {
         // Implement Dijkstra's Algorithm
         var shortestPaths = new Dictionary<Node, (int Distance, List<string> Path)>();
@@ -40,6 +40,58 @@ public class ShortestPathService : IShortestPathService
         var endNode = graphNodes.First(n => n.Name == toNodeName);
         return shortestPaths.ContainsKey(endNode) ?
             new ShortestPathResponseDto { NodeNames = shortestPaths[endNode].Path, Distance = shortestPaths[endNode].Distance } :
+            null;
+    }
+
+    public ShortestPathResponseDto ShortestPath(string fromNodeName, string toNodeName, List<Node> graphNodes)
+    {
+        var shortestPaths = new Dictionary<Node, (int Distance, List<string> Path, List<PathSegmentDto> Segments)>();
+        var unvisitedNodes = new List<Node>(graphNodes);
+
+        var startNode = graphNodes.First(n => n.Name == fromNodeName);
+        shortestPaths[startNode] = (0, new List<string> { startNode.Name }, new List<PathSegmentDto>());
+
+        while (unvisitedNodes.Count > 0)
+        {
+            var currentNode = unvisitedNodes
+                .Where(n => shortestPaths.ContainsKey(n))
+                .OrderBy(n => shortestPaths[n].Distance)
+                .FirstOrDefault();
+
+            if (currentNode == null || currentNode.Name == toNodeName)
+                break;
+
+            unvisitedNodes.Remove(currentNode);
+
+            foreach (var edge in currentNode.Edges)
+            {
+                var neighbor = edge.TargetNode;
+                var newDist = shortestPaths[currentNode].Distance + edge.Weight;
+                var newSegments = new List<PathSegmentDto>(shortestPaths[currentNode].Segments)
+                {
+                    new PathSegmentDto
+                    {
+                        FromNode = currentNode.Name,
+                        ToNode = neighbor.Name,
+                        Distance = edge.Weight
+                    }
+                };
+
+                if (!shortestPaths.ContainsKey(neighbor) || newDist < shortestPaths[neighbor].Distance)
+                {
+                    shortestPaths[neighbor] = (newDist, new List<string>(shortestPaths[currentNode].Path) { neighbor.Name }, newSegments);
+                }
+            }
+        }
+
+        var endNode = graphNodes.First(n => n.Name == toNodeName);
+        return shortestPaths.ContainsKey(endNode) ?
+            new ShortestPathResponseDto
+            {
+                NodeNames = shortestPaths[endNode].Path,
+                Distance = shortestPaths[endNode].Distance,
+                PathSegments = shortestPaths[endNode].Segments
+            } :
             null;
     }
 }
